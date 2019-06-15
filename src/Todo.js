0b400298a1280ab888c2b3c45318a64c74d7c4be
Peter <phefland@gmail.com>
2019-06-15 01:28:42 -0300

    added styling

--- src/Todo.js
+++ src/Todo.js
@@ -1,56 +1,82 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./Todo.css";
+import { FaPen } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
 
 class Todo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isEditing:false,
-            task: this.props.task
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false,
+      task: this.props.task
+    };
+  }
 
-    handleRemove = () => {
-        this.props.removeTodo(this.props.id);
-    }
+  handleRemove = () => {
+    this.props.removeTodo(this.props.id);
+  };
 
-    toggleForm = () => {
-        this.setState({ isEditing: !this.setState.isEditing }); 
-    }
+  toggleForm = () => {
+    this.setState({ isEditing: !this.setState.isEditing });
+  };
 
-    handleUpdate = (evt) => {
-        evt.preventDefault();
-        this.props.updateTodo(this.props.id, this.state.task);
-        this.setState({isEditing: false});
-    }
+  handleUpdate = evt => {
+    evt.preventDefault();
+    this.props.updateTodo(this.props.id, this.state.task);
+    this.setState({ isEditing: false });
+  };
 
-    handleChange = (evt) => {
-        this.setState({
-            [evt.target.name]: evt.target.value
-        });
+  handleChange = evt => {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  };
+
+  handleToggle = evt => {
+    this.props.toggleTodo(this.props.id);
+  };
+
+  render() {
+    let result;
+    if (this.state.isEditing) {
+      result = (
+        <div className='Todo'>
+          <form className='Todo-edit-form' onSubmit={this.handleUpdate}>
+            <input
+              type='text'
+              value={this.state.task}
+              name='task'
+              onChange={this.handleChange}
+            />
+            <button>Save</button>
+          </form>
+        </div>
+      );
+    } else {
+      result = (
+        <div className='Todo'>
+          <li
+            className={
+              this.props.completed ? "Todo-task completed" : "Todo-task"
+            }
+            onClick={this.handleToggle}
+          >
+            {" "}
+            {this.props.task}
+          </li>
+          <div className='Todo-buttons'>
+            <button onClick={this.toggleForm}>
+              <FaPen />
+            </button>
+            <button onClick={this.handleRemove}>
+              <FaTrash />
+            </button>
+          </div>
+        </div>
+      );
     }
-    
-    render() {
-        let result;
-        if (this.state.isEditing){
-            result = (
-                <div>
-                    <form onSubmit={this.handleUpdate}>
-                        <input type="text" value={this.state.task} name="task" onChange={this.handleChange} />
-                        <button>Save</button>
-                    </form>
-                </div>
-            );
-        } else {
-            result = (
-                <div>
-                    <button onClick={this.toggleForm}>Edit</button>
-                    <button onClick={this.handleRemove}>X</button>
-                    <li>{this.props.task}</li>
-                </div>
-            );
-        }
-        return result;
-    }
+    return result;
+  }
 }
 
 export default Todo;
